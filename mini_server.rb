9f5a6a3846b5d6d6028ff90f21061258176f5e00
João Luiz de Almeida Filho <jlalmeidaf@ig.com.br>
2011-09-13 22:38:58 -0300

    criado mini-servidor

--- mini_server.rb
+++ mini_server.rb
@@ -1,7 +1,10 @@
 require 'drb'
 
 class Mini_server
-  def calcular_resultado_de(expressao)
-    eval(expressao)
+  def calcular_resultado_de expressao
+    eval expressao
+  end
+  def cadastrar_no_servidor_principal 
+    true
   end
 end
