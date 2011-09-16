e0f02687625a278a78d4aa223717ef069a10ba9e
João Luiz de Almeida Filho <jlalmeidaf@ig.com.br>
2011-09-16 15:36:19 -0300

    Novo servidor

--- server.rb
+++ server.rb
@@ -1,1 +1,16 @@
 require 'drb'
+
+class Server
+  def initialize port
+    @port = port.to_s
+    DRb.start_service "druby://127.0.0.1:" << @port, self
+    DRb.thread.join 
+  end
+  def calculate member
+    eval member    
+  end
+end
+
+
+server = Server.new 1236
+
--- server_spec.rb
+++
@@ -1,8 +0,0 @@
-require './mini_server.rb'
-describe Mini_server do
-  it 'Calcular Resultado de expressoes' do
-    servidor = Mini_server.new
-    resultado = servidor.calcular_resultado_de "(2+3*5)+(6*2-3)"
-    resultado.should == 26
-  end
-end
