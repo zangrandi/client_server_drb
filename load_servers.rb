eaaea8301b79b692a96a70d5a4882224f2a36e1a
Augusto <zangrandii@gmail.com>
2011-09-16 18:57:49 -0300

    some changes

--- load_servers.rb
+++ load_servers.rb
@@ -11,10 +11,10 @@ DRb.start_service "druby://127.0.0.1:#{server.port}", server
 server.register_on domain_server
 p "servidor 1 iniciado"
 
+
 server2 = Server.new 2016
 DRb.start_service "druby://127.0.0.1:#{server2.port}", server2
 server.register_on domain_server
 p "servidor 2 iniciado"
-
 
 DRb.thread.join 
